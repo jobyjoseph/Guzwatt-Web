92dc074c667d0198a83bfc733162fabf39823b5d
Joby Joseph <reachjoby@gmail.com>
2024-02-05 11:41:08 -0800

    added logo

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,11 +1,20 @@
 import "@fontsource/poppins";
 import "@fontsource/poppins/700.css";
+import Head from "next/head";
 import styles from "./layout.module.scss";
 
 export function RootLayout({ children }: { children: any }) {
   return (
-    <div className={styles?.rootContainer}>
-      <div className={styles?.childWrapper}>{children}</div>
-    </div>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      <div className={styles?.rootContainer}>
+        <div className={styles?.logoWrapper}>
+          <img src="/logo.png" className={styles?.logo} alt="Guzwatt logo" />
+        </div>
+        <div className={styles?.childWrapper}>{children}</div>
+      </div>
+    </>
   );
 }
